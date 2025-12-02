d8b299de5f0b5a877ea90f9f81f1e9cb9f7567a8
Francesco Ilario <filario@redhat.com>
2025-12-02 18:00:28 +0100

    latest things
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- create-aws-secrets.sh
+++ create-aws-secrets.sh
@@ -6,7 +6,14 @@ ssh-keygen -t rsa -b 4096 -N "" -f '/tmp/ssh-mpc/id_rsa'
 kubectl create secret generic 'aws-ssh-key' \
   --from-file=id_rsa='/tmp/ssh-mpc/id_rsa' \
   --namespace 'multi-platform-controller'
+kubectl label secret 'aws-ssh-key' \
+  'build.appstudio.redhat.com/multi-platform-secret=true' \
+  --namespace 'multi-platform-controller'
+
 kubectl create secret generic 'aws-account' \
   --from-literal=access-key-id="${AWS_ACCESS_KEY_ID}" \
   --from-literal=secret-access-key="${AWS_SECRET_ACCESS_KEY}" \
+  --namespace 'multi-platform-controller'
+kubectl label secret 'aws-account' \
+  'build.appstudio.redhat.com/multi-platform-secret=true' \
   --namespace 'multi-platform-controller'
